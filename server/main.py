0dc593eeb7cd3805abb5d6da7c6f763caeae92c8
fawind <windheuser.fabian@gmail.com>
2017-12-01 23:03:16 +0100

    Remove deprecated image batch api

--- server/main.py
+++ server/main.py
@@ -16,21 +16,6 @@ class Home(webapp2.RequestHandler):
         self.redirect(GITHUB_HOME)
 
 
-class DeprecatedImageBatch(webapp2.RequestHandler):
-    """TODO: Remove after clients have updated"""
-
-    def __init__(self, *args, **kwargs):
-        super(DeprecatedImageBatch, self).__init__(*args, **kwargs)
-        self._artworks = Artworks('data/old-images.csv')
-        self._default_batch_size = 10
-
-    def get(self, index):
-        index = int(index) if index else 0
-        batch = self._artworks.get_batch(index, self._default_batch_size)
-        self.response.headers['Content-Type'] = 'application/json'
-        self.response.write(json.dumps(batch))
-
-
 class ImageBatch(webapp2.RequestHandler):
 
     def __init__(self, *args, **kwargs):
@@ -47,7 +32,6 @@ class ImageBatch(webapp2.RequestHandler):
 
 application = webapp2.WSGIApplication([
     ('/ping', Ping),
-    ('/api/v1/image/batch/(\d*)', DeprecatedImageBatch),
     ('/api/v2/image/batch/(\d*)', ImageBatch),
     ('/', Home)
 ], debug=False)
